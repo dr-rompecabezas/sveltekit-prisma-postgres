0ab3c0458488c172abe6edb97eb293e4666bfa70
dr-rompecabezas <felavid@gmail.com>
2021-12-14 23:40:12 +0000

    Add PATCH endpoint to update text.

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -8,8 +8,8 @@
     <button aria-label="Mark done/not done" class="toggle" />
   </form>
 
-  <form action="" method="" class="text">
-    <input type="text" value={todo.text} />
+  <form action="/todos/{todo.uid}.json?_method=patch" method="post" class="text">
+    <input type="text" name="text" value={todo.text} />
     <button aria-label="Save todo" class="save" />
   </form>
 
--- src/routes/todos/[uid].json.ts
+++ src/routes/todos/[uid].json.ts
@@ -4,4 +4,9 @@ import { api } from "./_api"
 export const del: RequestHandler = (request) => {
     return api(request)
   }
+
+export const patch: RequestHandler<{}, FormData> = (request) => {
+  const todo = {text: request.body.get("text")}
+  return api(request, todo)
+} 
   
