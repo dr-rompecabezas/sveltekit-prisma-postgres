d86ef604370c4aea487219d9865789dd87ccfbe1
dr-rompecabezas <felavid@gmail.com>
2021-12-14 14:30:50 +0000

    Style input fields.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,51 @@
 <script>
-    import TodoItem from "$lib/todo-item.svelte";
+  import TodoItem from "$lib/todo-item.svelte";
 
-    const title = "Todo | New";
+  const title = "Todo";
 </script>
 
 <svelte:head>
-    <title>{title}</title>
+  <title>{title}</title>
 </svelte:head>
 
-<h1>{title}</h1>
+<div class="todos">
+  <h1>{title}</h1>
 
-<form action="" method="">
-    <input type="text" name="text" aria-label="Add a todo" placeholder="+ tap to add a todo" /> 
-</form>
+  <form action="" method="" class="new">
+    <input
+      type="text"
+      name="text"
+      aria-label="Add a todo"
+      placeholder="+ tap to add a todo"
+    />
+  </form>
 
-<TodoItem />
-<TodoItem />
-<TodoItem />
+  <TodoItem />
+  <TodoItem />
+  <TodoItem />
+</div>
+
+<style>
+  .todos {
+    width: 100%;
+    max-width: 42rem;
+    margin: 4rem auto 0 auto;
+  }
+  
+  .new {
+    margin: 0 0 0.5rem 0;
+  }
+
+  .new input {
+    font-size: 28px;
+    width: 100%;
+    padding: 0.5em 1em 0.3em 1em;
+    box-sizing: border-box;
+    border-radius: 8px;
+    text-align: center;
+  }
+
+  .todos :global(input) {
+    border: 1px solid transparent;
+  }
+</style>
